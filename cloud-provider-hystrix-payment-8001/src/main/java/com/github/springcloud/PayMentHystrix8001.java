14fd142cbfa8097b9c65affb3af0cad245d53efd
ouyangxiaobo <2482330075@qq.com>
2020-03-30 13:48:56 +0800

    服务者整合断路器hystri模拟正常超时，超时，运行时异常

--- cloud-provider-hystrix-payment-8001/src/main/java/com/github/springcloud/PayMentHystrix8001.java
+++ cloud-provider-hystrix-payment-8001/src/main/java/com/github/springcloud/PayMentHystrix8001.java
@@ -2,6 +2,7 @@ package com.github.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  **/
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class PayMentHystrix8001 {
 
     public static void main(String[] args) {
