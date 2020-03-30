08ef40c7f4d8fbcfa38b251130040e2a4a861696
ouyangxiaobo <2482330075@qq.com>
2020-03-30 15:53:32 +0800

    添加网关服务

---
+++ cloud-gateway-gateway-9527/src/main/java/com/github/springcloud/config/GateWayConfig.java
@@ -0,0 +1,27 @@
+package com.github.springcloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class GateWayConfig
+{
+
+
+
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder)
+    {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+
+        routes.route("path_route_gateway",
+                r -> r.path("/guonei")
+                        .uri("http://news.baidu.com/guonei")).build();
+
+        return routes.build();
+    }
+}
