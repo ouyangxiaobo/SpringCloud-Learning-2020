f5d3f600d0f983a6f4eeb421e18f3ebed8b5fd7c
ouyangxiaobo <2482330075@qq.com>
2020-03-30 15:08:22 +0800

    添加Hystrix Dashboard进行实时监控

---
+++ cloud-consumer-hystrix-dashboard-9001/src/main/java/com/github/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,18 @@
+package com.github.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * author: ouyang
+ * Date:2020/3/30 15:01
+ **/
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001
+{
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
