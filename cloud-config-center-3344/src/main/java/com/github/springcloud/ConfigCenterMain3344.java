80e90b95a727d04c2ac99d80f951857d1d72b0e6
ouyangxiaobo <2482330075@qq.com>
2020-03-30 17:34:52 +0800

    springcloud-配置中心-config-server服务端

---
+++ cloud-config-center-3344/src/main/java/com/github/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,18 @@
+package com.github.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * author: ouyang
+ * Date:2020/3/30 16:46
+ **/
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344
+{
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
