9402f975ed988f5a9eacd31be9528644be447aad
ouyangxiaobo <2482330075@qq.com>
2020-03-30 19:59:13 +0800

    springcloud 配置中心客户端

---
+++ cloud-config-center-3355/src/main/java/com/github/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,18 @@
+package com.github.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * author: ouyang
+ * Date:2020/3/30 19:34
+ **/
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3355
+{
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
