426e4287fe349c96d8efb9e7d34330f45377ffb4
ouyangxiaobo <2482330075@qq.com>
2020-03-31 12:14:28 +0800

    添加消息驱动 cloud stream,并测试接受消息

---
+++ cloud-stream-rabbitmq-consumer-8802/src/main/java/com/github/springcloud/oontroller/ReceiveMessageListenerController.java
@@ -0,0 +1,36 @@
+package com.github.springcloud.oontroller;
+
+/**
+ * author: ouyang
+ * Date:2020/3/31 12:06
+ **/
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Component
+@EnableBinding(Sink.class)
+@Slf4j
+public class ReceiveMessageListenerController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+
+    @StreamListener(Sink.INPUT)
+    public void input(Message<String> message){
+
+        log.info("我是消费者1号，-----》接受到的消息是："+message.getPayload()+"\t"+serverPort);
+
+
+    }
+
+}
+
+
