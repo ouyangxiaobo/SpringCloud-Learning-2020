144daff52a4e4c19009b2e5a60394a3276abbc48
ouyangxiaobo <2482330075@qq.com>
2020-03-31 10:47:07 +0800

    消息消费者添加消息总线，一次修改，全部广播通知，处处生效

--- cloud-config-center-3355/src/main/java/com/github/springcloud/controller/ConfigClientController.java
+++ cloud-config-center-3355/src/main/java/com/github/springcloud/controller/ConfigClientController.java
@@ -5,10 +5,7 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @auther zzyy
- * @create 2020-02-21 18:08
- */
+
 @RestController
 @RefreshScope
 public class ConfigClientController
