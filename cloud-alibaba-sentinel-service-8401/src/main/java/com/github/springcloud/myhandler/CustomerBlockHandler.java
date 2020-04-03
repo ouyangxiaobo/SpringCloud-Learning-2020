d87bebc6833996bf7c024ed0017b4cbac4a1c557
ouyangxiaobo <2482330075@qq.com>
2020-04-03 16:32:12 +0800

    自定义统一异常返回

---
+++ cloud-alibaba-sentinel-service-8401/src/main/java/com/github/springcloud/myhandler/CustomerBlockHandler.java
@@ -0,0 +1,18 @@
+package com.github.springcloud.myhandler;
+
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.github.springcloud.common.CommonResult;
+import com.github.springcloud.entities.Payment;
+
+
+public class CustomerBlockHandler
+{
+    public static CommonResult handlerException(BlockException exception)
+    {
+        return new CommonResult(4444,"按客戶自定义,global handlerException----1");
+    }
+    public static CommonResult handlerException2(BlockException exception)
+    {
+        return new CommonResult(4444,"按客戶自定义,global handlerException----2");
+    }
+}
